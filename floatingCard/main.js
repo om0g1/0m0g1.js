319ab55b80949070dea640c4582bba0498502ba3
om0g1 <omogidavis@gmail.com>
2023-10-17 11:36:48 +0300

    card#2

---
+++ floatingCard/main.js
@@ -0,0 +1,8 @@
+import Atropos from 'https://cdn.jsdelivr.net/npm/atropos@2/atropos.min.mjs';
+
+const myAtropos = new Atropos({
+    el: '.atropos',
+    shadow: true,
+    shadowScale: 1.05,
+    highlights: true
+});
