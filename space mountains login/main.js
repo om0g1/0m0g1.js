e5ff0386d54f3dc322e2c9c6b130b0fa748f95a0
om0g1 <omogidavis@gmail.com>
2023-10-20 15:27:10 +0300

    shiningloader#1

---
+++ space mountains login/main.js
@@ -0,0 +1,37 @@
+const canvas = document.querySelector("#canvas");
+const pen = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+pen.shadowColor = "white";
+pen.shadowBlur = 10;
+pen.fillStyle = "white";
+
+function drawStars() {
+
+    function drawStar() {
+        const star = {
+            x: getRndInteger(0, canvas.width),
+            y: getRndInteger(0, canvas.height),
+            width: getRndInteger(1, 4)
+        }
+        pen.beginPath();
+        pen.arc(star.x, star.y, star.width, 0, 2 * Math.PI);
+        pen.fill();
+        setTimeout(() => {
+            pen.clearRect(star.x -( star.width * 5), star.y - (star.width * 5), star.width * 9, star.width * 9);
+        }, getRndInteger(500, 1000));
+    }
+
+    setTimeout(() => {
+        drawStar();
+        drawStars();
+    }, getRndInteger(10, 100));
+}
+
+drawStars();
